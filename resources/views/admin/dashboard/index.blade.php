0d851224cae96fed05c4ac7077bb709cb0d302aa
“windydestiny2” <windy.destiny@gmail.com>
2025-04-22 23:23:17 +0700

    10 Halaman login not done yet

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,3 @@
+<div class="container-fluid mt-2">
+<div class="alert alert-success">Halo {{  auth()->user()->name }}! Selamat Datang di Halaman Admin</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 // Ensure AdminUserController exists and is correctly imported
 
 
+Route::get('/login', [AdminAuthController::class, 'index']);
+Route::post('/login/do', [AdminAuthController::class, 'doLogin']);
+Route::get('/logout', [AdminAuthController::class, 'logout']);
+
 Route::get('/', function () {
     $data = [
         'content' => 'admin.dashboard.index' 
@@ -14,7 +18,17 @@ Route::get('/', function () {
 });
 
 
+
+
+
+
 Route::prefix('/admin')->group(function () {
+    Route::get('/dashboard', function () {
+        $data = [
+            'content' => 'admin.dashboard.index' 
+        ];
+        return view('admin.layouts.wrapper', $data);
+    });
     Route::resource('/user', AdminUserController::class);
 });
 
